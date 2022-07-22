514ce283a5bebb05c7f3b9599d75d488a7408155
Adel Helal <adel.helal@gmail.com>
2022-07-22 11:33:07 +1000

    Create compose.sh

---
+++ docker/compose.sh
@@ -0,0 +1,10 @@
+# Port allocation
+# s3: 9000
+# kubernetes: 9080
+# airflow: 9082
+# spark: 9090
+# kafka: 9092
+docker-compose -f s3/docker-compose.yml up -d
+docker-compose -f kafka/docker-compose.yml up -d
+docker-compose -f spark/docker-compose.yml up -d
+docker-compose -f airflow/docker-compose.yml up -d
