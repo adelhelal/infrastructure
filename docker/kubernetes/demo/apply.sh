fc303f60875c42f5ddc6a459cbd4dacdcc6ef307
Adel Helal <adel.helal@gmail.com>
2022-07-22 11:39:58 +1000

    Create apply.sh

---
+++ docker/kubernetes/demo/apply.sh
@@ -0,0 +1,8 @@
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+
+# kubectl logs [pod-name]
+# kubectl exec -it [pod-name] -- bin/bash
+
+# kubectl delete -f deployment.yaml
+# kubectl delete -f service.yaml
