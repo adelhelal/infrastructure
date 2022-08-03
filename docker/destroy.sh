22b9e7f2bfac60e0ff7e460bd9dbe9261165e0bc
Adel Helal <adel.helal@gmail.com>
2022-08-03 15:35:53 +1000

    Create destroy.sh

---
+++ docker/destroy.sh
@@ -0,0 +1,4 @@
+docker-compose -f kafka/docker-compose.yml down
+docker-compose -f s3/docker-compose.yml down
+docker-compose -f spark/docker-compose.yml down
+docker-compose -f airflow/docker-compose.yml down
