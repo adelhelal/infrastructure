44034d5ca486e724d7e8c24173b2b75c8dc2fd6e
Adel Helal <adel.helal@gmail.com>
2022-08-03 15:32:28 +1000

    Update apply.sh

--- docker/kubernetes/demo/apply.sh
+++ docker/kubernetes/demo/apply.sh
@@ -1,8 +1,27 @@
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+kubectl apply -f secret.yaml
+kubectl apply -f database-deployment.yaml
+kubectl apply -f database-service.yaml
 
-# kubectl logs [pod-name]
-# kubectl exec -it [pod-name] -- bin/bash
+kubectl apply -f configMap.yaml
+kubectl apply -f web-deployment.yaml
+kubectl apply -f web-service.yaml
 
 # kubectl delete -f deployment.yaml
 # kubectl delete -f service.yaml
+
+# kubectl get all
+
+## pod
+# kubectl get pod
+# kubectl get pod -o wide
+# kubectl describe pod [pod-name]
+# kubectl logs [pod-name]
+# kubectl exec -it [pod-name] -- bin/bash
+
+## service
+# kubectl get service
+# kubectl describe service [service-name]
+# minikube service [service-name] # assigns external IP to service
+
+## namespace
+# kubectl create namespace [namespace-name]
