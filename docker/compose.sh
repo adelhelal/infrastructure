772c3d83b0f6e90d29497d7f820c3840da0413ef
Adel Helal <adel.helal@gmail.com>
2022-08-04 10:40:40 +1000

    new line test with codespace

--- docker/compose.sh
+++ docker/compose.sh
@@ -4,6 +4,7 @@
 # airflow: 9082
 # spark: 9090
 # kafka: 9092
+
 docker-compose -f s3/docker-compose.yml up -d
 docker-compose -f kafka/docker-compose.yml up -d
 docker-compose -f spark/docker-compose.yml up -d
