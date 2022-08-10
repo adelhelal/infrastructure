fdaa160991a4e4ffc8fba1f49845425141c0dbb4
Adel Helal <adel.helal@gmail.com>
2022-08-10 13:30:15 +1000

    Update docker.sh

--- docker/metorikku/docker.sh
+++ docker/metorikku/docker.sh
@@ -7,10 +7,10 @@ mkdir -p /metorikku/data/output/
 docker cp data/schema/schema.json spark:/metorikku/data/schema/
 docker cp data/json/data.json spark:/metorikku/data/json/
 
-docker cp ingestion_job.yaml spark:/metorikku/
-docker cp ingestion_metric.yaml spark:/metorikku/
+docker cp job.yaml spark:/metorikku/
+docker cp metric.yaml spark:/metorikku/
 
 # https://github.com/YotpoLtd/metorikku/releases/tag/v0.0.151/
 docker cp metorikku-standalone_2.11.jar spark:/metorikku/
 
-spark-submit --class com.yotpo.metorikku.Metorikku /metorikku/metorikku-standalone_2.11.jar -c /metorikku/ingestion_job.yaml
+spark-submit --class com.yotpo.metorikku.Metorikku /metorikku/metorikku-standalone_2.11.jar -c /metorikku/job.yaml
