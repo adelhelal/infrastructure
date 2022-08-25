eb0e1bad3daa127e642ef4aee84f77c3b34016e0
Adel Helal <ahelal@tyro.com>
2022-08-25 11:43:52 +1000

    reference localhost for kafka

--- docker/kafka/consumer.sh
+++ docker/kafka/consumer.sh
@@ -1,1 +1,1 @@
-docker exec -it kafka kafka-console-consumer --bootstrap-server kafka:9092 --topic test_topic --from-beginning
+docker exec -it kafka kafka-console-consumer --bootstrap-server localhost:9092 --topic test_topic --from-beginning
--- docker/kafka/producer.sh
+++ docker/kafka/producer.sh
@@ -1,1 +1,1 @@
-docker exec -it kafka kafka-console-producer --bootstrap-server kafka:9092 --topic test_topic
+docker exec -it kafka kafka-console-producer --bootstrap-server localhost:9092 --topic test_topic
